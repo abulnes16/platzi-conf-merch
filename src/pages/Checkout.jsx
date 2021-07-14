b3625d75a4309d395cd6fb6e9dfc837e0be23243
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-07-14 09:52:42 -0600

    Layout checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import '../styles/components/Checkout.css';
 
-const Checkout = () => {
-  return <h1>Checkout</h1>;
-};
+const Checkout = () => (
+  <div className="checkout">
+    <div className="checkout-content">
+      <h3>Lista de Pedidos:</h3>
+      <div className="checkout-item">
+        <div className="checkout-element">
+          <h4>Item Name</h4>
+          <span>$10</span>
+        </div>
+        <button type="button"> Eliminar</button>
+      </div>
+    </div>
+    <div className="checkout-sidebar">
+      <h3>Precio Total: $10</h3>
+      <button type="button">Continuar pedido</button>
+    </div>
+  </div>
+);
 
 export default Checkout;
