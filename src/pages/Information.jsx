b3cd6590b25988138177cbd53af56baaeccb00e6
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-07-14 10:04:43 -0600

    Layout Information Page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import '../styles/components/Information.css';
 
-const Information = () => {
-  return <h1>Information</h1>;
-};
+const Information = () => (
+  <div className="information">
+    <div className="information-content">
+      <div className="information-head">
+        <h2>Información de contacto: </h2>
+      </div>
+      <div className="information-form">
+        <form>
+          <input type="text" placeholder="Nombre completo" name="name" />
+          <input type="email" placeholder="Correo electronico" name="email" />
+          <input type="text" placeholder="Dirección" name="address" />
+          <input type="text" placeholder="Apartamento" name="apto" />
+          <input type="text" placeholder="Ciudad" name="city" />
+          <input type="text" placeholder="País" name="country" />
+          <input type="text" placeholder="Estado" name="state" />
+          <input type="text" placeholder="Código Postal" name="cp" />
+          <input type="text" placeholder="Teléfono" name="phone" />
+        </form>
+      </div>
+      <div className="information-buttons">
+        <div className="information-back">Regresar</div>
+        <div className="information-next">Pagar</div>
+      </div>
+    </div>
+    <div className="information-sidebar">
+      <h3>Pedido:</h3>
+      <div className="information-item">
+        <div className="information-element">
+          <h4>Item Name</h4>
+          <span>$10</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Information;
